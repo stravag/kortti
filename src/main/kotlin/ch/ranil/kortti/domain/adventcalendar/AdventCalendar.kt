ce4793ec7986d40643e3de8515b69475b5a0948d
Ranil Wijeyratne <me@ranil.ch>
2023-11-15 23:10:01 +0100

    adventcalendar model

--- src/main/kotlin/ch/ranil/kortti/domain/adventcalendar/AdventCalendar.kt
+++ src/main/kotlin/ch/ranil/kortti/domain/adventcalendar/AdventCalendar.kt
@@ -4,9 +4,22 @@ import ch.ranil.kortti.domain.Id
 import ch.ranil.kortti.domain.IdFactory
 import java.util.*
 
-data class AdventCalendar(
+class AdventCalendar(
     val id: AdventCalendarId,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AdventCalendar
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
 
 @JvmInline
 value class AdventCalendarId(override val value: UUID) : Id {
