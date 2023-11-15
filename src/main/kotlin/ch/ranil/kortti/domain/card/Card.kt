c302336c9b9cc07ad4445e0ff20ccf6f796ea651
Ranil Wijeyratne <me@ranil.ch>
2023-11-15 22:47:07 +0100

    shift code to model object

--- src/main/kotlin/ch/ranil/kortti/domain/card/Card.kt
+++ src/main/kotlin/ch/ranil/kortti/domain/card/Card.kt
@@ -4,11 +4,47 @@ import ch.ranil.kortti.domain.Id
 import ch.ranil.kortti.domain.IdFactory
 import java.time.LocalDateTime
 import java.util.*
+import kotlin.random.Random
 
-data class Card(
+class Card(
     val id: CardId,
-    val entries: List<CardEntry> = emptyList()
-)
+    entries: List<CardEntry> = emptyList()
+) {
+    var entries: List<CardEntry> = entries
+        private set
+
+    fun addEntry() {
+        val charPool: List<Char> = ('a'..'z') + ('A'..'Z')
+        val randomString = (1..10)
+            .map { Random.nextInt(0, charPool.size).let { charPool[it] } }
+            .joinToString("")
+
+        val entry = CardEntry(
+            id = CardEntryId.random(),
+            dateTime = LocalDateTime.now(),
+            text = "Hello $randomString"
+        )
+
+        entries += entry
+    }
+
+    fun deleteEntry(cardEntryId: CardEntryId) {
+        entries = entries.filterNot { it.id == cardEntryId }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Card
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
 
 data class CardEntry(
     val id: CardEntryId,
--- src/main/kotlin/ch/ranil/kortti/domain/card/CardService.kt
+++ src/main/kotlin/ch/ranil/kortti/domain/card/CardService.kt
@@ -1,8 +1,5 @@
 package ch.ranil.kortti.domain.card
 
-import java.time.LocalDateTime
-import kotlin.random.Random
-
 class CardService(
     private val cardRepository: CardRepository,
 ) {
@@ -20,35 +17,15 @@ class CardService(
 
     fun addEntryToCard(cardId: CardId): Card {
         val card = requireNotNull(cardRepository.find(cardId))
-
-        val charPool: List<Char> = ('a'..'z') + ('A'..'Z')
-        val randomString = (1..10)
-            .map { Random.nextInt(0, charPool.size).let { charPool[it] } }
-            .joinToString("")
-
-        val entry = CardEntry(
-            id = CardEntryId.random(),
-            dateTime = LocalDateTime.now(),
-            text = "Hello $randomString"
-        )
-
-        val extendedCard = card.copy(
-            entries = card.entries + entry
-        )
-
-        cardRepository.save(extendedCard)
-
-        return extendedCard
+        card.addEntry()
+        cardRepository.save(card)
+        return card
     }
 
     fun deleteEntryFromCard(cardId: CardId, cardEntryId: CardEntryId): Card {
         val card = requireNotNull(cardRepository.find(cardId))
-        val extendedCard = card.copy(
-            entries = card.entries.filterNot { it.id == cardEntryId }
-        )
-
-        cardRepository.save(extendedCard)
-
-        return extendedCard
+        card.deleteEntry(cardEntryId)
+        cardRepository.save(card)
+        return card
     }
 }
