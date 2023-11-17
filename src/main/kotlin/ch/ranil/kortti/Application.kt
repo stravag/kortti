ab3d3ddb9a0dc79659315b8b11d53cbac0712fbe
Ranil Wijeyratne <me@ranil.ch>
2023-11-17 14:27:13 +0100

    extract methods

--- src/main/kotlin/ch/ranil/kortti/Application.kt
+++ src/main/kotlin/ch/ranil/kortti/Application.kt
@@ -8,6 +8,7 @@ import ch.ranil.kortti.domain.card.CardService
 import ch.ranil.kortti.persistence.AdventCalendarRepositoryImpl
 import ch.ranil.kortti.persistence.CardRepositoryImpl
 import ch.ranil.kortti.web.configureRoutes
+import gg.jte.ContentType
 import gg.jte.TemplateEngine
 import gg.jte.resolve.ResourceCodeResolver
 import io.ktor.server.application.*
@@ -16,36 +17,43 @@ import io.ktor.server.jte.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
-import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
 fun main() {
     embeddedServer(
-        factory = Netty,
-        port = 9001,
-        host = "0.0.0.0"
+        factory = Netty, port = 9001, host = "0.0.0.0"
     ) {
-        install(Koin) {
-            modules(appModule)
-        }
-        install(Jte) {
-            val resolver = ResourceCodeResolver("templates", javaClass.classLoader)
-            templateEngine = TemplateEngine.create(resolver, gg.jte.ContentType.Html)
-        }
-        install(StatusPages) {
-            exception<EntityNotFoundException> { call, _ ->
-                call.respond(JteContent("error/404.kte", emptyMap()))
-            }
-        }
+        configureDependencyInjection()
+        configureTemplating()
+        configureErrorHandling()
         configureRoutes()
     }.start(wait = true)
 }
 
-private val appModule: Module
-    get() = module {
-        single<AdventCalendarRepository> { AdventCalendarRepositoryImpl() }
-        single { AdventCalendarService(get()) }
-        single<CardRepository> { CardRepositoryImpl() }
-        single { CardService(get()) }
+private fun Application.configureErrorHandling() {
+    install(StatusPages) {
+        exception<EntityNotFoundException> { call, _ ->
+            call.respond(JteContent("error/404.kte", emptyMap()))
+        }
     }
+}
+
+private fun Application.configureTemplating() {
+    install(Jte) {
+        val resolver = ResourceCodeResolver("templates", javaClass.classLoader)
+        templateEngine = TemplateEngine.create(resolver, ContentType.Html)
+    }
+}
+
+private fun Application.configureDependencyInjection() {
+    install(Koin) {
+        modules(module {
+            single<AdventCalendarRepository> { AdventCalendarRepositoryImpl() }
+            single { AdventCalendarService(get()) }
+            single<CardRepository> { CardRepositoryImpl() }
+            single { CardService(get()) }
+        })
+    }
+}
+
