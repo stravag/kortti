0d4ef4c6974bddaef557a3de8e951b78e1525ab7
Ranil Wijeyratne <me@ranil.ch>
2025-04-07 23:24:19 +0200

    working spring example

--- src/main/kotlin/ch/ranil/kortti/web/Controller.kt
+++ src/main/kotlin/ch/ranil/kortti/web/Controller.kt
@@ -1,9 +1,11 @@
 package ch.ranil.kortti.web
 
+import ch.ranil.kortti.domain.card.CardEntryId
+import ch.ranil.kortti.domain.card.CardId
 import ch.ranil.kortti.domain.card.CardService
 import ch.ranil.kortti.templates.Templates
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 class Controller(
@@ -12,8 +14,40 @@ class Controller(
 ) {
 
     @GetMapping("/")
-    fun rootPage(): String {
+    fun cardOverviewPage(): String {
         val cards = cardService.getCards()
         return templates.pageCardOverview(cards).render()
+    }
+
+    @GetMapping("/cards/{cardId}")
+    fun cardDetailPage(@PathVariable("cardId") cardId: UUID): String {
+        val card = cardService.getCard(CardId(cardId))
+        return templates.pageCardDetail(card).render()
+    }
+
+    @GetMapping("/card-form")
+    fun cardForm(): String {
+        return templates.componentCardForm(null).render()
+    }
+
+    @PostMapping("/cards")
+    fun createCard(@RequestBody data: CardFormData): String {
+        val card = cardService.createCard(data)
+        return templates.componentCardRow(card).render()
+    }
+
+    @PostMapping("/cards/{cardId}/entries")
+    fun createCardEntry(@PathVariable("cardId") cardId: UUID): String {
+        val card = cardService.addEntryToCard(CardId(cardId))
+        return templates.pageCardDetail(card).render()
+    }
+
+    @DeleteMapping("/cards/{cardId}/entries/{cardEntryId}")
+    fun deleteCardEntry(
+        @PathVariable("cardId") cardId: UUID,
+        @PathVariable("cardEntryId") cardEntryId: UUID
+    ): String {
+        val card = cardService.deleteEntryFromCard(CardId(cardId), CardEntryId(cardEntryId))
+        return templates.pageCardDetail(card).render()
     }
 }
