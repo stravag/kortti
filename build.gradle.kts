a4b9647ce85c7665d342d3f4048a4de2c34fbe8f
Ranil Wijeyratne <me@ranil.ch>
2026-02-05 23:14:42 +0100

    upgrade to htmx4 alpha

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,9 @@ dependencies {
     implementation("gg.jte:jte:3.2.2")
     jteGenerate("gg.jte:jte-models:3.2.2")
 
-    implementation("org.webjars.npm:htmx.org:2.0.8")
+    implementation("org.webjars.npm:htmx.org:4.0.0-alpha6")
     implementation("org.webjars.npm:picocss__pico:2.1.1")
 
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
