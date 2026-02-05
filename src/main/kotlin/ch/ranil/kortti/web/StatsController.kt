ee97aa42f268a893d5a95eb4cc447f3224e8f3d5
Ranil Wijeyratne <me@ranil.ch>
2026-02-06 00:10:42 +0100

    add sse

--- src/main/kotlin/ch/ranil/kortti/web/StatsController.kt
+++ src/main/kotlin/ch/ranil/kortti/web/StatsController.kt
@@ -4,6 +4,10 @@ import ch.ranil.kortti.templates.Templates
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import java.util.concurrent.Executors
+import kotlin.random.Random
+
 
 @RestController
 @RequestMapping("/stats")
@@ -13,5 +17,29 @@ class StatsController(
     @GetMapping
     fun page(): String {
         return templates.pageStats().render()
+    }
+
+    @GetMapping("/load-stream")
+    fun loadStream(): SseEmitter {
+        val emitter = SseEmitter()
+        val sseMvcExecutor = Executors.newSingleThreadExecutor()
+        sseMvcExecutor.execute {
+            try {
+                var progress = 0
+                do {
+                    val event = SseEmitter.event()
+                        .data(templates.componentStatsProgress(progress).render())
+                    emitter.send(event)
+                    progress += Random.nextInt(0, 10)
+                    Thread.sleep(Random.nextLong(100, 500))
+                } while (progress < 100)
+                val event = SseEmitter.event()
+                    .data("<img src=\"https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmN4ZjB4MWVicGduNmttajdteDM0MWdpZDQzdW50cmNhdng2ODB5eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/yYSSBtDgbbRzq/giphy.gif\">")
+                emitter.send(event)
+            } catch (ex: Exception) {
+                emitter.completeWithError(ex)
+            }
+        }
+        return emitter
     }
 }
