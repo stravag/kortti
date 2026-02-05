8624ae2232c4fadf166e78f4830182e31dc5d775
Ranil Wijeyratne <me@ranil.ch>
2026-02-06 00:55:36 +0100

    start spring container

--- src/test/kotlin/ch/ranil/kortti/CardsPageTest.kt
+++ src/test/kotlin/ch/ranil/kortti/CardsPageTest.kt
@@ -1,33 +1,56 @@
 package ch.ranil.kortti
 
 import ch.ranil.kortti.domain.card.CardType
+import com.microsoft.playwright.Browser
 import com.microsoft.playwright.BrowserType
 import com.microsoft.playwright.Page
 import com.microsoft.playwright.Page.GetByRoleOptions
 import com.microsoft.playwright.Playwright
 import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
 import com.microsoft.playwright.options.AriaRole
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
 
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CardsPageTest {
+
+    @LocalServerPort
+    private var port: Int = 0
+
+    private lateinit var playwright: Playwright
+    private lateinit var browser: Browser
+    private lateinit var page: Page
+
+    @BeforeEach
+    fun setUp() {
+        playwright = Playwright.create()
+        browser = playwright.chromium().launch(
+            BrowserType.LaunchOptions()
+                .setHeadless(false)
+                .setSlowMo(300.0)
+        )
+        val context = browser.newContext()
+        page = context.newPage()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        // Essential to close resources to prevent memory leaks/zombie browsers
+        browser.close()
+        playwright.close()
+    }
+
     @Test
     fun `can create and delete card`() {
-        Playwright.create().use { playwright ->
-            val browser = playwright.chromium().launch(
-                BrowserType.LaunchOptions()
-                    .setHeadless(false)
-                    .setSlowMo(300.0)
-            )
-            val context = browser.newContext()
-            val page: Page = context.newPage()
-            page.navigate("http://localhost:8080/")
-            page.addCard("test1")
-            page.addCard("test2")
-            page.addCard("test3")
+        page.navigate("http://localhost:$port/")
+        page.addCard("test1")
+        page.addCard("test2")
+        page.addCard("test3")
 
-            page.deleteCard("test2")
-        }
+        page.deleteCard("test2")
     }
 
     private fun Page.addCard(name: String, type: CardType? = null) {
